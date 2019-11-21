17ef625da269799d07e7c028fef2fe6c0bece77a
mpadge <mark.padgham@email.com>
2019-11-21 14:01:31 +0100

    add fn-defs-rstudio

---
+++ function-defs-rstudio.R
@@ -0,0 +1,51 @@
+# in lieu of an "official" list of packages, this function gets all the package
+# names that they deign as deserving of a hex sticker
+get_rstudio_repos <- function () {
+    u <- "https://github.com/rstudio/hex-stickers/tree/master/PNG"
+    x <- httr::GET (u) %>%
+        httr::content (as = "parsed") %>%
+        rvest::html_table ()
+    repos <- gsub ("\\.png", "", x [[1]]$Name)
+    repos <- repos [(grep ("RStudio", repos) + 1):length (repos)]
+    repos <- gsub ("\\-female", "", repos) # "plumber-female"
+    repos <- repos [repos != "pipe"] # "magrittr"
+    return (repos)
+}
+
+# github orgs are not necessarily rstudio. This function gets the org names from
+# looking at the websites listed on CRAN as generated from the DESCRIPTION files
+get_org <- function (repo) {
+    u <- paste0 ("https://cran.r-project.org/package=", repo)
+    x <- httr::GET (u) %>%
+        httr::content (as = "parsed", encoding = "UTF-8") %>%
+        rvest::html_table ()
+    urls <- as.vector (x [[1]] [x [[1]] [, 1] == "URL:", ] [, 2]) %>%
+        strsplit (",")
+    if (!any (grepl ("github", urls [[1]])))
+    { # no github URLs; look in BugReports
+        urls <- as.vector (x [[1]] [x [[1]] [, 1] == "BugReports:", ] [, 2]) %>%
+            strsplit (",")
+    }
+    if (length (urls) == 0)
+        return ("")
+
+    ret <- ""
+    if (any (grepl ("github\\.com", urls [[1]])))
+    {
+        urls <- urls [[1]] [grep ("github\\.com", urls [[1]])]
+        if (length (urls) > 0)
+            ret <- strsplit (strsplit (urls, "github\\.com\\/") [[1]] [2],
+                             "\\/") [[1]] [1]
+    } else if (any (grepl ("github\\.io", urls [[1]])))
+    {
+        urls <- urls [[1]] [grep ("\\.github\\.io", urls [[1]])]
+        if (length (urls) > 0)
+            ret <- strsplit (strsplit (urls, "\\.github\\.io") [[1]] [1],
+                             "https:\\/\\/") [[1]] [2]
+    }
+
+    return (ret)
+}
+
+# repos <- get_rstudio_repos ()
+# orgs <- vapply (repos, function (i) get_org (i), character (1))
